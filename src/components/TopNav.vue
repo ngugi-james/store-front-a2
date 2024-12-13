f04f85a8906bccac23ee2c9efed3aec8830dc0d3
James Ngugi <53422918+ngugi-james@users.noreply.github.com>
2024-12-12 21:06:21 -0500

    Update CI/CD workflow

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/BestBuyLogo.jpg" alt="Best Buy Store Logo">
       </a>
     </div>
     <button class="hamburger" @click="toggleNav">
